27cb2a68a872b9b0d47fb28188f9dc2fa02e3f68
deepanshut041 <deepanshut041@gmail.com>
2018-12-23 02:05:40 +0530

    Completed basic register form now working on validation and completion

--- client/src/app/main/main.module.ts
+++ client/src/app/main/main.module.ts
@@ -2,31 +2,40 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule, MatCheckboxModule, MatInputModule, MatIconModule, MatSelectModule, MatOptionModule, MatFormFieldModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 // Core components
 import { MainComponent } from './main.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
-
+// HomeComponents
 import { HomeComponent } from './home/home.component';
 import { LandingPageComponent } from './home/landing-page/landing-page.component';
 import { EventsOverviewComponent } from './home/events-overview/events-overview.component';
 import { TeamOverviewComponent } from './home/team-overview/team-overview.component';
+
+// Events Component
 import { EventsComponent } from './events/events.component';
 import { EventsListComponent } from './events/events-list/events-list.component';
 import { EventsDetailsComponent } from './events/events-details/events-details.component';
+
+// Register Components
+import { RegsiterComponent } from "./register/register.component";
 
 import { MainService } from "./main.service";
 
 
 @NgModule({
   imports: [
-    CommonModule, RouterModule, HttpClientModule
+    CommonModule, RouterModule, HttpClientModule, FormsModule, ReactiveFormsModule,
+     MatButtonModule, MatCheckboxModule, MatInputModule, BrowserAnimationsModule, MatIconModule, MatSelectModule, MatOptionModule, MatFormFieldModule
   ],
   declarations: [
     MainComponent, NavbarComponent, FooterComponent, HomeComponent, LandingPageComponent, EventsOverviewComponent,
-    TeamOverviewComponent, EventsComponent, EventsListComponent, EventsDetailsComponent
+    TeamOverviewComponent, EventsComponent, EventsListComponent, EventsDetailsComponent, RegsiterComponent
   ],
   providers:[MainService,]
 })
