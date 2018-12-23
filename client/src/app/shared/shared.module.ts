0185c3e674de33c558a2de2fe0885b510adb21d0
Sarthak23khatana <sarthak.khatana97@gmail.com>
2018-12-23 21:33:03 +0530

    Update shared module

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ConnectionErrorComponent } from './connection-error/connection-error.component';
+import { LoaderComponent } from './loader/loader.component';
+import { FormResultComponent } from './form-result/form-result.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PageNotFoundComponent]
+  declarations: [PageNotFoundComponent, ConnectionErrorComponent, LoaderComponent, FormResultComponent]
 })
 export class SharedModule { }
