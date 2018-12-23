5aa257e1211b174ab1d3ee0a838a406703266605
deepanshut041 <deepanshut041@gmail.com>
2018-12-23 20:47:51 +0530

    Added three.js in file

--- client/src/app/main/home/landing-page/landing-page.component.ts
+++ client/src/app/main/home/landing-page/landing-page.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-// import { LandingPageService } from './landing-page.service';
+import { LandingPageService } from './landing-page.service';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-home-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _landingPage:LandingPageService) { }
 
   ngOnInit() {
+    var scene3d = document.getElementById("scene3d");
+    this._landingPage.threeActivator(scene3d)
   }
+
 
 }
