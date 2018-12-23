1d28fc7da6c707940a2ed00ebc700dbabbcd5cb4
deepanshut041 <deepanshut041@gmail.com>
2018-12-23 22:52:13 +0530

    fixed frontend prod issues

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {DOMAIN, ASSETS} from "./assets"
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PageNotFoundComponent]
+  declarations: [PageNotFoundComponent],
+  exports:[]
 })
 export class SharedModule { }
