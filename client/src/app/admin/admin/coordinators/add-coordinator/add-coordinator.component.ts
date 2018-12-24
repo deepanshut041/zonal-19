a7e257729fa09ca3b9a4a0e7a5dd2fa399907040
prabhanshuchauhan <prabhanshu.chauhan98@gmail.com>
2018-12-24 16:20:59 +0530

    Work on admin

---
+++ client/src/app/admin/admin/coordinators/add-coordinator/add-coordinator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-coordinator',
+  templateUrl: './add-coordinator.component.html',
+  styleUrls: ['./add-coordinator.component.css']
+})
+export class AddCoordinatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
