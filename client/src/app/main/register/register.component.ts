e82e5740bdd6eca3f73496a5c3c657a7f5cba3e0
deepanshut041 <deepanshut041@gmail.com>
2018-12-26 21:20:55 +0530

    Completed registration form now working on errors

--- client/src/app/main/register/register.component.ts
+++ client/src/app/main/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators, ReactiveFormsModule, FormArray } from "@angular/forms";
 import { MainService } from "../main.service";
+import { ASSETS } from 'src/app/shared/assets';
 
 @Component({
   selector: 'app-regsiter',
@@ -8,13 +9,22 @@ import { MainService } from "../main.service";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit, AfterViewInit {
+
+  LOADER_IMAGE = ASSETS + "/Loader.svg"
   registerForm: FormGroup
+  particpantsForm: FormGroup
+  recaptchaForm: FormGroup
   events: any[]
   max_participants: number
   total_participants: number
   participants: any[]
   SITE_KEY: string = "6Ld-VYQUAAAAAKb_EMJhaBY1sCFhOz_OZEpMaxrK"
-  
+  form_progress = false;
+  form_success = false;
+  form_error = false;
+  form_success_message = "Successfully registered"
+  form_error_message = "Please check your information correctly and try again"
+  form_show = true;
 
   constructor(private fb: FormBuilder, private _mainService: MainService) {
     let emailFormat = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
@@ -28,10 +38,34 @@ export class RegisterComponent implements OnInit, AfterViewInit {
       'faculty_gender': [null, Validators.compose([Validators.required])],
       'faculty_phn_no': [null, Validators.compose([Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern(numberFormat)])],
       'faculty_email': [null, Validators.compose([Validators.required, Validators.pattern(emailFormat)])],
-      'event': [null, Validators.compose([Validators.required])],
+      'event': [null, Validators.compose([Validators.required])]
+    });
+
+    this.particpantsForm = fb.group({
       'participants': this.fb.array([]),
-      'n_participants': [null, Validators.compose([Validators.required])],
+      'n_participants': [null, Validators.compose([Validators.required])]
+    })
+
+    this.recaptchaForm = fb.group({
       'recaptcha': [null, Validators.compose([Validators.required])]
+    })
+
+  }
+
+  createItem(): FormGroup {
+    let emailFormat = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
+    let nameFormat = "[a-zA-Z\s]+$";
+    let numberFormat = '^[0-9]*$';
+    return this.fb.group({
+      'name': [null, Validators.compose([Validators.required, Validators.maxLength(50), Validators.minLength(3), Validators.pattern(nameFormat)])],
+      'fathers_name': [null, Validators.compose([Validators.required, Validators.maxLength(50), Validators.minLength(3), Validators.pattern(nameFormat)])],
+      'university_roll': [null, Validators.compose([Validators.required, Validators.maxLength(15), Validators.minLength(10), Validators.pattern(numberFormat)])],
+      'branch': [null, Validators.compose([Validators.required, Validators.maxLength(50), Validators.minLength(3), Validators.pattern(nameFormat)])],
+      'year': [null, Validators.compose([Validators.required])],
+      'gender': [null, Validators.compose([Validators.required])],
+      'aadhar_no': [null, Validators.compose([Validators.required, Validators.maxLength(16), Validators.minLength(16), Validators.pattern(numberFormat)])],
+      'phn_no': [null, Validators.compose([Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern(numberFormat)])],
+      'email': [null, Validators.compose([Validators.required, Validators.pattern(emailFormat)])]
     });
   }
 
@@ -49,11 +83,11 @@ export class RegisterComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     this.registerForm.controls['event'].valueChanges.subscribe(value => {
       this.max_participants = parseInt(this.getEvent(value));
-      this.registerForm.controls['n_participants'].setValue(0);
+      this.particpantsForm.controls['n_participants'].setValue(0);
       this.participants = this.addParticipantsList(this.max_participants);
     })
 
-    this.registerForm.controls['n_participants'].valueChanges.subscribe(value => {
+    this.particpantsForm.controls['n_participants'].valueChanges.subscribe(value => {
       this.total_participants = parseInt(value);
       this.resetParticipants();
       this.addParticipants(this.total_participants);
@@ -71,14 +105,14 @@ export class RegisterComponent implements OnInit, AfterViewInit {
   }
 
   addParticipants(maxp: number): void {
-    let control = <FormArray>this.registerForm.controls['participants']
+    let control = <FormArray>this.particpantsForm.controls['participants']
     for (let i = control.length; i < maxp; i++) {
       control.push(this.createItem())
     }
   }
 
   resetParticipants(): void {
-    let control = <FormArray>this.registerForm.controls['participants']
+    let control = <FormArray>this.particpantsForm.controls['participants']
 
     if (this.total_participants < control.length) {
       while (this.total_participants != control.length) {
@@ -100,43 +134,43 @@ export class RegisterComponent implements OnInit, AfterViewInit {
   }
 
 
-  createItem(): FormGroup {
-    let emailFormat = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
-    let nameFormat = "[a-zA-Z\s]+$";
-    let numberFormat = '^[0-9]*$';
-    return this.fb.group({
-      'name': [null, Validators.compose([Validators.required, Validators.maxLength(50), Validators.minLength(3), Validators.pattern(nameFormat)])],
-      'fathers_name': [null, Validators.compose([Validators.required, Validators.maxLength(50), Validators.minLength(3), Validators.pattern(nameFormat)])],
-      'university_roll': [null, Validators.compose([Validators.required, Validators.maxLength(15), Validators.minLength(10), Validators.pattern(numberFormat)])],
-      'branch': [null, Validators.compose([Validators.required, Validators.maxLength(50), Validators.minLength(3), Validators.pattern(nameFormat)])],
-      'year': [null, Validators.compose([Validators.required])],
-      'gender': [null, Validators.compose([Validators.required])],
-      'aadhar_no': [null, Validators.compose([Validators.required, Validators.maxLength(16), Validators.minLength(16), Validators.pattern(numberFormat)])],
-      'phn_no': [null, Validators.compose([Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern(numberFormat)])],
-      'email': [null, Validators.compose([Validators.required, Validators.pattern(emailFormat)])]
-    });
-  }
-
   submitForm() {
-    if (this.registerForm.valid) {
-      console.log("Valid Form")
-      this._mainService.regsiter(this.registerForm.value).subscribe(
-        (resp) => {
-          this.registerForm.reset();
+    if (this.registerForm.valid && this.particpantsForm.valid && this.recaptchaForm.valid) {
+      this.form_progress = true;
+      this.form_show = false;
+      this.form_error = false;
+      let values = Object.assign({}, this.registerForm.value, this.particpantsForm.value, this.recaptchaForm.value);
+      this._mainService.regsiter(values).subscribe(
+        (success) => {
+          this.form_progress = false;
+          this.form_success = true;
+          setTimeout(() => {    //<<<---    using ()=> syntax
+            this.registerForm.reset();
+            this.resetParticipants();
+            this.particpantsForm.reset();
+            this.recaptchaForm.reset();
+            this.form_success = false;
+          }, 5000);
+
         }, (err) => {
-          console.log(err);
+
+          this.form_error = true;
+          this.form_progress = false;
+          this.form_show = true;
+          this.form_error_message = "Please check your information correctly and try again";
         }
       )
-    }else{
-      console.log("Invalid Form")
+    } else {
+      this.form_error = true;
+      this.form_error_message = "Don't try to hack you idiot, Tum se na ho paya";
     }
   }
 
   resolved(captchaResponse: string) {
-    this.registerForm.controls['recaptcha'].setValue(captchaResponse)
+    this.recaptchaForm.controls['recaptcha'].setValue(captchaResponse)
   }
 
   getControls() {
-    return <FormArray>this.registerForm.get('participants');
+    return <FormArray>this.particpantsForm.get('participants');
   }
 }
